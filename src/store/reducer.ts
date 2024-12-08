712ec6c81ac9d9a63058dc87690f0a9e7b5e10ff
factor-feerist <erik.blodoex@gmail.com>
2024-12-08 21:43:59 +0500

    module7-task1

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,14 +1,19 @@
 import { createReducer } from '@reduxjs/toolkit';
 import {
   setCurrentCity,
-  setOffers
+  setOffers,
+  loadOffers,
+  requireAuthorization,
+  setOffersDataLoadingStatus
 } from './action';
 import { Offer } from '../types/offer';
-import { Cities, CityName } from '../const';
+import { AuthorizationStatus, Cities, CityName } from '../const';
 
 const initialState = {
   activeCity: Cities[CityName.Paris],
-  offers: [] as Offer[]
+  offers: [] as Offer[],
+  isOffersDataLoading: false,
+  AuthorizationStatus: AuthorizationStatus.Unknown
 };
 
 export const reducer = createReducer(initialState, (builder) => {
@@ -18,5 +23,14 @@ export const reducer = createReducer(initialState, (builder) => {
     })
     .addCase(setOffers, (state, action) => {
       state.offers = action.payload;
+    })
+    .addCase(loadOffers, (state, action) => {
+      state.offers = action.payload;
+    })
+    .addCase(setOffersDataLoadingStatus, (state, action) => {
+      state.isOffersDataLoading = action.payload;
+    })
+    .addCase(requireAuthorization, (state, action) => {
+      state.AuthorizationStatus = action.payload;
     });
 });
